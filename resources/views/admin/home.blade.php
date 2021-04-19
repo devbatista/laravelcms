338d9a0b2d18b5315ae264523f4557c35c562ea8
Rafael Batista <rafael@devbatista.com>
2021-04-19 18:49:03 -0300

    Configurando inicialmente o AdminLTE

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,19 @@
-ADMIN
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Painel de Controle</h1>
+@endsection
+
+@section('content')
+    ...
+@endsection
+
+@section('css')
+{{-- Colocar importações de CSS --}}
+@endsection
+
+@section('js')
+{{-- Colocar arquivos js (fica no fim da página) --}}
+@endsection
