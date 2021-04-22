16d83c80c010150765f6311ce5f236e09e85c18f
Rafael Batista <rafael@devbatista.com>
2021-04-22 11:25:51 -0300

    Adicionando paginação

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,23 +10,36 @@
 @endsection
 
 @section('content')
-    <table class="table table-hover">
-        <tr>
-            <th>ID</th>    
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Ações</th>
-        </tr>
-        @foreach ($users as $user)
-            <tr>
-                <td>{{$user->id}}</td>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>
-                    <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="btn btn-sm btn-warning">Editar</a>
-                    <a href="{{ route('users.destroy', ['user' => $user->id]) }}" class="btn btn-sm btn-danger">Excluir</a>
-                </td>
-            </tr>
-        @endforeach
-    </table>  
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <a href="{{ route('users.edit', ['user' => $user->id]) }}"
+                                    class="btn btn-sm btn-warning">Editar</a>
+                                <a href="{{ route('users.destroy', ['user' => $user->id]) }}"
+                                    class="btn btn-sm btn-danger">Excluir</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    {{ $users->links() }}
+
 @endsection
