a8bc46c38bab38a75c36b519a6eed4d70915277f
Rafael Batista <rafael@devbatista.com>
2021-04-27 18:11:51 -0300

    Validando os dados editados

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use App\Setting;
+use Symfony\Component\VarDumper\Caster\RedisCaster;
 
 class SettingController extends Controller
 {
@@ -16,7 +18,7 @@ class SettingController extends Controller
     public function index()
     {
         $settings = [];
-        
+
         $dbsettings = Setting::get();
 
         foreach ($dbsettings as $dbsetting) {
@@ -25,5 +27,28 @@ class SettingController extends Controller
 
         $data = ['settings' => $settings];
         return view('admin.settings.index', $data);
+    }
+
+    public function save(Request $request)
+    {
+        $data = $request->only(['title', 'subtitle', 'email', 'bgcolor', 'textcolor']);
+        $validator = $this->validator($data);
+
+        if ($validator->fails()) {
+            return redirect()->route('settings')->withErrors($validator);
+        }
+
+        // return redirect()->route('settings');
+    }
+
+    private function validator($data)
+    {
+        return Validator::make($data,[
+            'title' => ['string', 'max:100'],
+            'subtitle' => ['string', 'max:100'],
+            'email' => ['string', 'email'],
+            'bgcolor' => ['string', 'regex:/#[A-Z0-9]{6}/i'],
+            'textcolor' => ['string', 'regex:/#[A-Z0-9]{6}/i'],
+        ]);
     }
 }
--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -7,43 +7,54 @@
 @endsection
 
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                <h5><i class="icon fas fa-ban"></i> Ocorreu um erro.</h5>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div class="card">
         <div class="card-body">
-            <form action="{{route('settings.save')}}" method="POST" class="form-horizontal">
+            <form action="{{ route('settings.save') }}" method="POST" class="form-horizontal">
                 @method('PUT')
                 @csrf
 
                 <div class="form-group row">
                     <label for="" class="col-sm-2 col-form-label">Título do site</label>
                     <div class="col-sm-10">
-                        <input type="text" name="title" value="{{$settings['title']}}" class="form-control">
+                        <input type="text" name="title" value="{{ $settings['title'] }}" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="" class="col-sm-2 col-form-label">Subtítulo</label>
                     <div class="col-sm-10">
-                        <input type="text" name="subtitle" value="{{$settings['subtitle']}}" class="form-control">
+                        <input type="text" name="subtitle" value="{{ $settings['subtitle'] }}" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="" class="col-sm-2 col-form-label">Email para contato</label>
                     <div class="col-sm-10">
-                        <input type="email" name="email" value="{{$settings['email']}}" class="form-control">
+                        <input type="email" name="email" value="{{ $settings['email'] }}" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="" class="col-sm-2 col-form-label">Cor do fundo</label>
                     <div class="col-sm-10">
-                        <input type="color" name="bgcolor" value="{{$settings['bgcolor']}}" class="form-control">
+                        <input type="color" name="bgcolor" value="{{ $settings['bgcolor'] }}" class="form-control">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="" class="col-sm-2 col-form-label">Cor do text</label>
                     <div class="col-sm-10">
-                        <input type="color" name="textcolor" value="{{$settings['textcolor']}}" class="form-control">
+                        <input type="color" name="textcolor" value="{{ $settings['textcolor'] }}" class="form-control">
                     </div>
-                </div>              
-                
+                </div>
+
                 <div class="form-group row">
                     <label for="" class="col-sm-2 col-form-label"></label>
                     <div class="col-sm-10">
