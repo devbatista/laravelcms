2c82cde3f1e9d97a52f5fb0680fc2cfa3028f123
Rafael Batista <rafael@devbatista.com>
2021-04-30 12:52:07 -0300

    Edição de páginas criado

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -46,7 +46,7 @@ class PageController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->only(['title','body']);
+        $data = $request->only(['title', 'body']);
         $data['slug'] = Str::slug($data['title'], '-');
 
         $validator = Validator::make($data, [
@@ -87,7 +87,13 @@ class PageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $page = Page::find($id);
+
+        if ($page) {
+            return view('admin.pages.edit', ['page' => $page]);
+        }
+
+        return redirect()->route('pages.index');
     }
 
     /**
@@ -99,7 +105,40 @@ class PageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $page = Page::find($id);
+        if ($page) {
+            $data = $request->only(['title', 'body']);
+
+            if ($page['title'] !== $data['title']) {
+                $data['slug'] = Str::slug($data['title'], '-');
+
+                $validator = Validator::make($data, [
+                    'title' => ['required', 'string', 'max:100'],
+                    'body' => ['string'],
+                    'slug' => ['required', 'string', 'max:100', 'unique:pages'],
+                ]);
+            } else {
+                $validator = Validator::make($data, [
+                    'title' => ['required', 'string', 'max:100'],
+                    'body' => ['string'],
+                ]);
+            }
+
+            if ($validator->fails()) {
+                return redirect()->route('pages.edit', ['page' => $id])->withErrors($validator)->withInput();
+            }
+
+            $page->title = $data['title'];
+            $page->body = $data['body'];
+
+            if (!empty($data['slug'])) {
+                $page->slug = $data['slug'];
+            }
+            
+            $page->save();
+        }
+
+        return redirect()->route('pages.index');
     }
 
     /**
