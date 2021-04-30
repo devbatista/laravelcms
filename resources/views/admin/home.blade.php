d581c45d1cd174f9c63a11adbcb5a093c7f0f0a7
Rafael Batista <rafael@devbatista.com>
2021-04-30 17:27:09 -0300

    Iniciando a estrutura do dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,9 +3,77 @@
 @section('title', 'Painel')
 
 @section('content_header')
-    <h1>Painel de Controle</h1>
+    <h1>Dashboard</h1>
 @endsection
 
 @section('content')
-    ...
+    <div class="row">
+        <div class="col-md-3">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Visitantes</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-eye"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Usuários online</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-heart"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Páginas</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-sticky-note"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Usuários</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-user"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Páginas mais visitadas</h3>
+                </div>
+                <div class="card-body">
+                    ...
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Sobre o sistema</h3>
+                </div>
+                <div class="card-body">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
